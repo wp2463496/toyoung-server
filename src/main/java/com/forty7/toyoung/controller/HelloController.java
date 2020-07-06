47fe4116b9cdcc78de0184303892c34587555140
wangping <xiaowangboke@vip.qq.com>
2020-07-06 17:59:35 +0800

    新增环境配置管理

--- src/main/java/com/forty7/toyoung/controller/HelloController.java
+++ src/main/java/com/forty7/toyoung/controller/HelloController.java
@@ -1,5 +1,7 @@
 package com.forty7.toyoung.controller;
 
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,6 +9,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/hello")
 public class HelloController {
+
+    // 当前环境配置名称
+    @Value("${profile.name}") //读取当前环境配置名称
+    private String profileName;
+
 
     @RequestMapping(value="/hello1", method= RequestMethod.GET)
     public String hello1() {
@@ -25,4 +32,11 @@ public class HelloController {
 
         return "Hello3!";
     }
+
+    @ApiOperation(value="获取当前环境", notes="获取当前环境接口")
+    @RequestMapping(value="/profileName", method = RequestMethod.GET, produces="application/json")
+    public String getProfileName() {
+        return "当前环境：" + profileName;
+    }
+
 }
